6d95292f7dc842cd80b44baf8b65ab57f698d3ac
Alex Shaffer <alex@cleverkraft.com>
2017-06-22 20:19:04 +0200

    Added bridging to support running on ComputerCraft as well as local.

--- nrg.lua
+++ nrg.lua
@@ -10,8 +10,11 @@ function nrg.constInit()
   
   nrg.const = {}
   
-  nrg.const.bigReactorMaxEnergyStorage = 10000000
+  nrg.const.BRmaxEnergyStorage = 10000000
   nrg.const.turbineRPMmode = { off = 0, half = 900 , full = 1800, free = -1 }
+  nrg.const.BRreactor = "BigReactors-Reactor"
+  nrg.const.BRturbine = "BigReactors-Turbine"
+  nrg.const.MKcube = "mekanism-machine"
   
 end
 
@@ -21,43 +24,70 @@ function nrg.configInit()
   nrg.status = {}
   
   nrg.config.enabled = true
-  nrg.status.validGrid = false
+  nrg.config.CC = (_CC_VERSION ~= nil)
+  nrg.status.gridValid = false
 
+end
+
+function nrg.bridgingInit()
+  
+  nrg.fn = {}
+  
+  if nrg.config.CC then
+    
+    nrg.fn.findReactors = function() return {peripheral.find(nrg.const.BRreactor)} end
+    nrg.fn.findTurbines = function() return {peripheral.find(nrg.const.BRturbine)} end
+    nrg.fn.findStorage = function() return {peripheral.find(nrg.const.MKcube)} end
+    
+  else
+    
+    nrg.fn.findReactors = function() return {} end
+    nrg.fn.findTurbines = function() return {} end
+    nrg.fn.findStorage = function() return {} end
+    
+  end
+  
 end
 
 
 -- Finds power equipment hooked up to the network
 function nrg.gridInit()
   
-  
   nrg.energyStorageMax = 0
   nrg.energyStorageCurrent = 0
   
-  nrg.reactors = gridInitReactorFind()
-  nrg.turbines = gridInitTurbineFind()
+  nrg.reactors = gridInitReactorsFind()
+  nrg.turbines = gridInitTurbinesFind()
   nrg.storage = gridInitStorageFind(nrg.turbines)
   
-  nrg.config.validGrid = (#nrg.reactors == 1) and (#nrg.turbines > 0)
+  nrg.config.gridValid = ((#nrg.reactors == 1) and (#nrg.turbines > 0))
+  
+  print("Grid summary:")
+  print("Reactors: "..#nrg.reactors)
+  print("Turbines: "..#nrg.turbines)
+  print("Storage: "..#nrg.storage)
+  print("Valid: "..tostring(nrg.config.gridValid))
+  
   
   nrg.gridUpdate()
 
 end
 
 
-function gridInitReactorFind()
+function gridInitReactorsFind()
   
-  local reactors = {}
-  
-  
+  local reactors = nrg.fn.findReactors()
+    
+    
   print("Reactors found: "..#reactors)
   
   return reactors
   
 end
 
-function gridInitTurbineFind()
+function gridInitTurbinesFind()
   
-  local turbines = {}
+  local turbines = nrg.fn.findTurbines()
   
   
   print("Turbines found: "..#turbines)
@@ -66,9 +96,9 @@ function gridInitTurbineFind()
   
 end
 
-function gridInitStorageFind()
+function gridInitStorageFind(turbines)
 
-  local storage = {}
+  local storage = nrg.fn.findStorage()
   
   
   print("Additional storage found: "..#storage)
@@ -89,12 +119,13 @@ end
 
 nrg.constInit()
 nrg.configInit()
+nrg.bridgingInit()
 nrg.gridInit()
 
-if nrg.gridValid then
+if nrg.config.gridValid then
   print("Valid grid.")
 else
-  print("Grid is not valid. Needs 1 reactors and at least 1 turbine.")
+  print("Grid is not valid. Needs 1 reactor and at least 1 turbine.")
 end
 
 
